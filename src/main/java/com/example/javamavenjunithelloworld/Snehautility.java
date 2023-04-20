7a36593df260c86178efbc724f136ae029d3c412
Sneha-0308 <mathadawar01@gmail.com>
2023-04-20 07:00:35 +0000

    utility code added

---
+++ src/main/java/com/example/javamavenjunithelloworld/Snehautility.java
@@ -0,0 +1,14 @@
+class Snehautility{
+static void sneha(int num){
+if(num%2==0){
+  System.out.println("It is even");
+}
+else{
+  System.out.println("It is odd");
+}
+}
+public static void main(String[] args){
+ sneha(100);
+}
+}
+
